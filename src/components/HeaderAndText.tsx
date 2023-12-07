22cafe9e150ec726253e5fee9ec85c620b39316a
luis-henrique-carvalho <luisdev9225@gmail.com>
2023-12-07 12:34:03 -0300

    :sparkles: feat: Add HeaderAndText component

---
+++ src/components/HeaderAndText.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props {
+  title: string;
+  text: string;
+}
+
+const HeaderAndText = ({ text, title }: Props) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <h1 className="text-h1">{title}</h1>
+      <p className="text-paragraph-base">{text}</p>
+    </div>
+  );
+};
+
+export default HeaderAndText;
