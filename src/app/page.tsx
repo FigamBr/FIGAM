6b58186f34e5a374b015ec78091c5bcb03e7a14e
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-01-02 17:26:14 -0300

    :recycle: refactor: Home component to use Prismic API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,21 @@
-import Circles from '@/components/Circles'
-import ComponentButton from '@/components/ComponentButton'
-import { PageUnderConstruction } from '@/components/PageUnderConstruction'
+
+import { createClient } from "@/prismicio";
 import React from 'react'
+import HeaderHome from './components/HeaderHome';
+import AboutHome from "./components/AboutHome";
 
-const Home = () => {
+const Home = async () => {
+  const prismic = createClient();
+  const home: any = await prismic.getByUID("home", "home_page_1");
+
+  const data = home.data
+  console.log(data)
   return (
-    <div className="flex flex-col w-full items-center gap-6">
+    <>
+      <HeaderHome data={data} />
+      <AboutHome data={data} />
+    </>
 
-      <div className='flex flex-col gap-3 items-center text-center'>
-        <div className='flex gap-4'>
-          <Circles />
-          <h2 className='font-normal text-2xl i'>FIGAM</h2>
-          <Circles />
-        </div>
-        <div className='flex flex-col gap-3'>
-          <h2 className=' text-base font-normal'>FUNDAÇÃO IRACI GAMA DE CULTURA</h2>
-          <p className=' text-xs font-light'>A Fundação Iraci Gama de Cultura (FIGAM) é uma instituição criada em 2002,
-            mas tem a memória de um quarto de século de pesquisa histórica sobre a
-            cidade de Alagoinhas e de trabalhos artísticos, culturais, eventos e criação
-            de instituições para benefício da comunidade.
-          </p>
-        </div>
-        <div className='flex gap-4'>
-          <ComponentButton text='Saiba mais' color='primary' link='/about' />
-          <ComponentButton text='Contato' color='secondary' link='/contact' />
-        </div>
-      </div>
-
-    </div>
   )
 }
 
