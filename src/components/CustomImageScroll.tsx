dcf87163e9c3e4884dece018c78a80866c9ca630
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-01-02 17:15:16 -0300

    :sparkles: feat: Add conditional rendering for title in CustomImageScroll component

--- src/components/CustomImageScroll.tsx
+++ src/components/CustomImageScroll.tsx
@@ -5,16 +5,18 @@ interface CustomImageScrollProps {
   src: string;
   alt: string;
   title: string;
+  home?: boolean;
 }
 
 const CustomImageScroll: React.FC<CustomImageScrollProps> = ({
   title,
   src,
   alt,
+  home
 }) => {
   return (
     <div className="relative w-40 lg:w-[300px] p-4 hover:size transition-transform duration-300 ease-in-out">
-      <div className=" flex flex-col w-40 h-44 lg:w-[300px] lg:h-[330px] gap-1">
+      <div className={`flex flex-col w-40 ${home ? "h-32": "h-48"} lg:w-[300px] lg:h-[330px] gap-1`}>
         <figure className="relative w-full h-full lg:mb-2">
           <Image
             src={src}
@@ -25,7 +27,8 @@ const CustomImageScroll: React.FC<CustomImageScrollProps> = ({
           />
         </figure>
       </div>
-      <p className="text-xs mb-1 lg:text-base font-light">{title}</p>
+      {!home && <p className="text-xs mb-1 lg:text-base font-light">{title}</p>}
+
     </div>
   );
 };
