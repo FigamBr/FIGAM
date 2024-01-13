d803041ed3c5fc786caae63329d94b53b2a487ef
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-01-13 18:25:54 -0300

    :recycle: refactor: News page component

--- src/app/(pages)/news/page.tsx
+++ src/app/(pages)/news/page.tsx
@@ -1,20 +1,29 @@
-import CircleHome from '@/components/CircleHome';
-import CustomImage from '@/components/CustomImage';
 import { createClient } from '@/prismicio';
-import Link from 'next/link';
 import React from 'react';
 import MainNew from './components/MainNew';
+import PostCard from './components/PostCard';
+import LastNewsHeader from './components/LastNewsHeader';
 
 const News = async () => {
   const prismic = createClient();
 
   const newsPage: any = await prismic.getByUID("news", "news_page_1");
-
-  console.log(newsPage);
+  const post: any = await prismic.getAllByType("post", {
+    pageSize: 10,
+    orderings: ["my.post.first_publication_date desc"]
+  });
 
   return (
     <div className="flex flex-col w-full gap-6">
-      <MainNew  newsPage={newsPage} />
+      <MainNew newsPage={newsPage} />
+
+      <LastNewsHeader />
+
+      <section className='flex flex-col gap-5 items-center'>
+        {post && post.map((post: any) => (
+          <PostCard post={post} />
+        ))}
+      </section>
     </div>
   );
 }
