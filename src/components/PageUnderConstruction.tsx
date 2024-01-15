649226a3a4622b238e104b07aa6125e5a6abd23f
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-01-15 01:29:34 -0300

    :sparkles: feat: Add links to "Noticias" and "Inicio" in PageUnderConstruction component

--- src/components/PageUnderConstruction.tsx
+++ src/components/PageUnderConstruction.tsx
@@ -8,6 +8,8 @@ export const PageUnderConstruction = () => {
         <div className='flex flex-col items-center justify-center gap-5'>
             <h2 className='text-3xl'>Paginas disponiveis no momento:</h2>
             <Link href="/about" className='text-2xl underline'>Sobre</Link>
+            <Link href="/news" className='text-2xl underline'>Noticias</Link>
+            <Link href="/" className='text-2xl underline'>Inicio</Link>
         </div>
     </div>
   )
