2a15a899106f1ea040b19e94b3d23861ca93bc52
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-03-25 10:04:49 -0300

    Refactor News component to handle error case

--- src/app/(pages)/news/page.tsx
+++ src/app/(pages)/news/page.tsx
@@ -5,26 +5,31 @@ import PostCard from './components/PostCard';
 import LastNewsHeader from './components/LastNewsHeader';
 
 const News = async () => {
-  const prismic = createClient();
-  const newsPage: any = await prismic.getByUID("news", "news_page_1");
-  const post: any = await prismic.getAllByType("post", {
-    pageSize: 10,
-    orderings: ["my.post.first_publication_date desc"]
-  });
+  try {
+    const prismic = createClient();
+    const newsPage: any = await prismic.getByUID("news", "news_page_1");
+    const post: any = await prismic.getAllByType("post", {
+      pageSize: 10,
+      orderings: ["my.post.first_publication_date desc"]
+    });
 
-  return (
-    <div className="flex flex-col w-full gap-6">
-      <MainNew newsPage={newsPage} />
+    return (
+      <div className="flex flex-col w-full gap-6">
+        <MainNew newsPage={newsPage} />
 
-      <LastNewsHeader />
+        <LastNewsHeader />
 
-      <section className='flex flex-col lg:flex-row lg:flex-wrap gap-5 items-center justify-between '>
-        {post && post.map((post: any) => (
-          <PostCard post={post} key={post.uid} />
-        ))}
-      </section>
-    </div>
-  );
+        <section className='flex flex-col lg:flex-row lg:flex-wrap gap-5 items-center justify-between '>
+          {post && post.map((post: any) => (
+            <PostCard post={post} key={post.uid} />
+          ))}
+        </section>
+      </div>
+    );
+  } catch (error) {
+    return <div className='text-center text-red-500'>Nenhuma notícia cadastrada</div>;
+  }
 }
+
 
 export default News;
